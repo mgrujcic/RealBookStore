5812e0891d245f7f4fc6652410cdc59107a6a575
mgrujcic <markogrujcic@hotmail.com>
2024-05-11 16:35:22 +0200

    Add authorization and fix CSRF on /myprofile

--- src/main/java/com/urosdragojevic/realbookstore/controller/BooksController.java
+++ src/main/java/com/urosdragojevic/realbookstore/controller/BooksController.java
@@ -3,6 +3,7 @@ package com.urosdragojevic.realbookstore.controller;
 import com.urosdragojevic.realbookstore.domain.*;
 import com.urosdragojevic.realbookstore.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -68,6 +69,7 @@ public class BooksController {
     }
 
     @GetMapping("/create-form")
+    @PreAuthorize("hasAuthority('CREATE_BOOK')") //moze da vidi stranicu samo ako sme da pravi knjigu
     public String CreateForm(Model model) {
         model.addAttribute("genres", genreRepository.getAll());
         return "create-form";
@@ -80,6 +82,7 @@ public class BooksController {
     }
 
     @PostMapping("/books")
+    @PreAuthorize("hasAuthority('VIEW_BOOKS_LIST')")
     public String createBook(NewBook book) {
         List<Genre> genreList = this.genreRepository.getAll();
         List<Genre> genresToInsert = book.getGenres().stream().map(bookGenreId -> genreList.stream().filter(genre -> genre.getId() == bookGenreId).findFirst().get()).collect(Collectors.toList());
--- src/main/java/com/urosdragojevic/realbookstore/controller/CommentController.java
+++ src/main/java/com/urosdragojevic/realbookstore/controller/CommentController.java
@@ -5,12 +5,14 @@ import com.urosdragojevic.realbookstore.domain.User;
 import com.urosdragojevic.realbookstore.repository.CommentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+
 public class CommentController {
     private static final Logger LOG = LoggerFactory.getLogger(CommentController.class);
 
@@ -21,6 +23,7 @@ public class CommentController {
     }
 
     @PostMapping(value = "/comments")
+    @PreAuthorize("hasAuthority('ADD_COMMENT')")
     public String createComment(@ModelAttribute Comment comment, Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         comment.setUserId(user.getId());
